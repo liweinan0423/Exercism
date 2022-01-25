adf6b4f9da6346e580f5f85c8e42282cede2b1a2
Weinan Li <liweinan0423@icloud.com>
2022-01-26 00:11:42 +0800

    --wip--

--- bash/markdown/markdown.sh
+++ bash/markdown/markdown.sh
@@ -15,135 +15,135 @@ declare STATE PREV_STATE
 declare LIST_STATE=closed
 
 main() {
-    exec <"$1"
-    while IFS= read -r LINE; do
-        process_line
-    done
-    process_line # process one more time in case there is empty line before EOF
-    echo "$OUTPUT"
+	exec <"$1"
+	while IFS= read -r LINE; do
+		process_line
+	done
+	process_line # process one more time in case there is no empty line before EOF
+	echo "$OUTPUT"
 }
 
 process_line() {
-    determine_state
-    process_inline_styles
+	determine_state
+	process_inline_styles
 
-    case $STATE in
-    end-of-list)
-        close_list
-        process_line
-        ;;
-    header)
-        parse_header
-        ;;
-    paragraph)
-        parse_paragraph
-        ;;
-    list)
-        parse_list
-        ;;
-    empty) ;;
-    *)
-        determine_state
-        process_line
-        ;;
-    esac
+	case $STATE in
+	end-of-list)
+		close_list
+		process_line
+		;;
+	header)
+		parse_header
+		;;
+	paragraph)
+		parse_paragraph
+		;;
+	list)
+		parse_list
+		;;
+	empty) ;;
+	*)
+		determine_state
+		process_line
+		;;
+	esac
 }
 
 determine_state() {
-    PREV_STATE=$STATE
-    case $LINE in
-    \#\#\#\#\#\#\#*) # only h1-h6 are allowed, others are parsed as regular paragraph
-        STATE=paragraph
-        ;;
-    \#*)
-        STATE=header
-        ;;
-    [\*-]*)
-        STATE=list
-        ;;
-    *([[:space:]]))
-        STATE=empty
-        ;;
-    *)
-        STATE=paragraph
-        ;;
-    esac
-    if [[ $PREV_STATE == list && $STATE != list ]]; then
-        STATE=end-of-list
-    fi
+	PREV_STATE=$STATE
+	case $LINE in
+	\#\#\#\#\#\#\#*) # only h1-h6 are allowed, others are parsed as regular paragraph
+		STATE=paragraph
+		;;
+	\#*)
+		STATE=header
+		;;
+	[\*-]*)
+		STATE=list
+		;;
+	*([[:space:]]))
+		STATE=empty
+		;;
+	*)
+		STATE=paragraph
+		;;
+	esac
+	if [[ $PREV_STATE == list && $STATE != list ]]; then
+		STATE=end-of-list
+	fi
 }
 
 ## below are actions for different states
 
 close_list() {
-    if [[ $LIST_STATE == open ]]; then
-        OUTPUT+="</ul>"
-        LIST_STATE=closed
-    fi
+	if [[ $LIST_STATE == open ]]; then
+		OUTPUT+="</ul>"
+		LIST_STATE=closed
+	fi
 }
 
 parse_header() {
-    local level content
-    if [[ $LINE =~ ^(\#+)\ +(.*) ]]; then
-        level=$((${#BASH_REMATCH[1]}))
-        content=${BASH_REMATCH[2]}
-        OUTPUT+="<h$level>$content</h$level>"
-    fi
+	local level content
+	if [[ $LINE =~ ^(\#+)\ +(.*) ]]; then
+		level=$((${#BASH_REMATCH[1]}))
+		content=${BASH_REMATCH[2]}
+		OUTPUT+="<h$level>$content</h$level>"
+	fi
 }
 
 parse_paragraph() {
-    OUTPUT+="<p>$LINE</p>"
+	OUTPUT+="<p>$LINE</p>"
 }
 
 parse_list() {
-    case $LIST_STATE in
-    closed)
-        OUTPUT+="<ul>"
-        LIST_STATE=open
-        parse_list
-        ;;
-    open)
-        to_listitem
-        OUTPUT+=$LINE
-        ;;
-    *)
-        LIST_STATE=closed
-        parse_list
-        ;;
-    esac
+	case $LIST_STATE in
+	closed)
+		OUTPUT+="<ul>"
+		LIST_STATE=open
+		parse_list
+		;;
+	open)
+		to_listitem
+		OUTPUT+=$LINE
+		;;
+	*)
+		LIST_STATE=closed
+		parse_list
+		;;
+	esac
 }
 
 to_listitem() {
-    printf -v LINE "<li>%s</li>" "${LINE#??}"
+	printf -v LINE "<li>%s</li>" "${LINE#??}"
 }
 
 process_inline_styles() {
-    parse_bold
-    parse_italics
+	parse_bold
+	parse_italics
 }
 
 parse_bold() {
-    while true; do
-        orig=$LINE
-        if [[ $LINE =~ ^(.+)__(.*) ]]; then
-            post=${BASH_REMATCH[2]}
-            pre=${BASH_REMATCH[1]}
-            if [[ $pre =~ ^(.*)__(.+) ]]; then
-                printf -v LINE "%s<strong>%s</strong>%s" "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "$post"
-            fi
-        fi
-        [ "$LINE" != "$orig" ] || break
-    done
+	while true; do
+		orig=$LINE
+		if [[ $LINE =~ ^(.+)__(.*) ]]; then
+			post=${BASH_REMATCH[2]}
+			pre=${BASH_REMATCH[1]}
+			if [[ $pre =~ ^(.*)__(.+) ]]; then
+				printf -v LINE "%s<strong>%s</strong>%s" "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "$post"
+			fi
+		fi
+		[ "$LINE" != "$orig" ] || break
+	done
 }
 
 parse_italics() {
-    while [[ $LINE == *_*?_* ]]; do
-        one=${LINE#*_}
-        two=${one#*_}
-        if [[ ${#two} -lt ${#one} && ${#one} -lt ${#LINE} ]]; then
-            printf -v LINE "%s" "${LINE%%_"$one"}<em>${one%%_"$two"}</em>$two"
-        fi
-    done
+	while [[ $LINE == *_*?_* ]]; do
+		one=${LINE#*_}
+		two=${one#*_}
+		if [[ ${#two} -lt ${#one} && ${#one} -lt ${#LINE} ]]; then
+			printf -v LINE "%s" "${LINE%%_"$one"}<em>${one%%_"$two"}</em>$two"
+		fi
+	done
 }
 
 main "$@"
