ab52f3d02f322a7ef3ff1149a9519e2944d73a33
Weinan Li <liweinan0423@icloud.com>
2022-02-11 16:49:26 +0800

    --wip--

--- bash/run-length-encoding/run_length_encoding.sh
+++ bash/run-length-encoding/run_length_encoding.sh
@@ -5,16 +5,7 @@ main() {
     local result
     local -i counter=1
     while read -rn1 current; do
-        if [[ $current == "$prev" ]]; then
-            ((counter++))
-        else
-            if ((counter == 1)); then
-                result+=$current
-            else
-                result+="$counter$prev"
-            fi
-        fi
-        prev=$current
+        result+=$current
     done < <(printf "%s" "$2")
 
     echo "$result"
