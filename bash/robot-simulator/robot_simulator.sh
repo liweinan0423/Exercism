65899f473486ae516a4e3dd655c997a14197ca4d
Weinan Li <liweinan0423@icloud.com>
2022-02-11 14:03:36 +0800

    --wip--

--- bash/robot-simulator/robot_simulator.sh
+++ bash/robot-simulator/robot_simulator.sh
@@ -3,11 +3,24 @@
 declare -ra Directions=(north east south west)
 declare north=0 east=1 south=2 west=3
 
+declare -i x y # coordinates
+declare dir    # direction
+
 main() {
-    local -i x=${1:-0} y=${2:-0}
-    local dir=${3:-north}
+    x=${1:-0} y=${2:-0}
+    dir=${3:-north}
+
     local instruction=$4
 
+    process "$instruction"
+    echo "$x $y $dir"
+}
+
+process() {
+
+    local instruction=$1
+
+    local idx
     case $instruction in
     R)
         idx=$((${!dir} + 1))
@@ -36,8 +49,6 @@ main() {
         esac
         ;;
     esac
-
-    echo "$x $y $dir"
 }
 
 main "$@"
