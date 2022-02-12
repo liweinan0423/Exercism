085d9c37684e971c58dbc5ca48b424666b4bef6f
Weinan Li <liweinan0423@icloud.com>
2022-02-12 23:50:25 +0800

    --wip--

--- bash/kindergarten-garden/kindergarten_garden.sh
+++ bash/kindergarten-garden/kindergarten_garden.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-declare -rA Students=(
-    [Alice]=0
-    [Bob]=1
-    [Charlie]=2
-    [David]=3
-    [Eve]=4
-    [Fred]=5
-    [Ginny]=6
-    [Harriet]=7
-    [Ileana]=8
-    [Joseph]=9
-    [Kincaid]=10
-    [Larry]=11
-)
-declare -A dict=(
-    [R]=radishes
-    [C]=clover
-    [V]=violets
-    [G]=grass
-)
-
 plant() {
     case $1 in
     R) echo radishes ;;
@@ -47,18 +26,18 @@ student() {
     esac
 }
 
-garden=$1 name=$2
+input=$1 name=$2
 while read -r line; do
     width=${#line}
     while read -rn1 plant; do
-        plants+=("$plant")
+        garden+=("$plant")
     done < <(printf "%s" "$line")
-done <<<"$garden"
+done <<<"$input"
 
 id=$(student "$name")
-positions=($((2 * id)) $((2 * id + 1)) $((width + 2 * id)) $((width + 2 * id + 1)))
-for i in "${positions[@]}"; do
-    output+=("$(plant "${plants[i]}")")
+offsets=($((2 * id)) $((2 * id + 1)) $((width + 2 * id)) $((width + 2 * id + 1)))
+for offset in "${offsets[@]}"; do
+    output+=("$(plant "${garden[offset]}")")
 done
 
 echo "${output[*]}"
