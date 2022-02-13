4af84a096b7b66ee4bdb138af9528736dd6b7e06
Weinan Li <liweinan0423@icloud.com>
2022-02-13 16:23:29 +0800

    --wip--

--- bash/yacht/yacht.sh
+++ bash/yacht/yacht.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2015
+
 main() {
     local category=$1
 
     shift
     case $category in
     yacht)
-        is_yacht "$@" &&
-            echo 50 ||
-            echo 0
+        is_yacht "$@" && yacht || zero
         ;;
+    ones) ;;
+
     esac
+
 }
 
 is_yacht() {
     (($1 == $2 && $1 == $3 && $1 == $4 && $1 == $5))
+}
+
+yacht() {
+    echo 50
+}
+
+zero() {
+    echo 0
 }
 
 main "$@"
