64e8a2fb02bf0c70cb2a6622db93b1bc36f7cc43
Weinan Li <liweinan0423@icloud.com>
2022-02-13 17:16:15 +0800

    --wip--

--- bash/yacht/yacht.sh
+++ bash/yacht/yacht.sh
@@ -43,13 +43,17 @@ is_four_of_a_kind() {
     is_combination "(1 4|4 1)" "$@" || is_yacht "$@"
 }
 
+parse() {
+    local -n __counter=$1
+    shift
+    for roll; do
+        __counter[$roll]=$((__counter[$roll] + 1))
+    done
+}
 four_of_a_kind() {
 
     local -A counter
-    for roll; do
-        counter[$roll]=$((counter[$roll] + 1))
-    done
-    [[ ${counter[*]} =~ (1 4|4 1|5) ]] || return 1
+    parse counter "$@"
 
     for roll in "${!counter[@]}"; do
         ((counter[$roll] >= 4)) && echo $((4 * roll)) && return
