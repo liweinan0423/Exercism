1f75f150b50536362e0f648bb68e75d19e943c83
Weinan Li <liweinan0423@icloud.com>
2022-02-15 13:52:25 +0800

    --wip--

--- bash/poker/poker.sh
+++ bash/poker/poker.sh
@@ -50,22 +50,6 @@ compare() {
 }
 
 compare_high_card() {
-    # local -a sorted1 sorted2
-    # sort_hand "$1" sorted1
-    # sort_hand "$2" sorted2
-    # winner=$(cat <(echo "${sorted1[@]%?}") <(echo "${sorted2[@]%?}") | sort -rn | head -n1)
-    # looser=$(cat <(echo "${sorted1[@]%?}") <(echo "${sorted2[@]%?}") | sort -rn | tail -n1)
-    # if [[ $winner == "$looser" ]]; then
-    #     echo tie
-    # elif [[ $winner == "${sorted1[*]%?}" ]]; then
-    #     echo win
-    # else
-    #     echo loose
-    # fi
-    compare_high_card2 "$@"
-}
-
-compare_high_card2() {
     local -a hand1 hand2
     read -ra hand1 <<<"$(hand::parse "$1")"
     read -ra hand2 <<<"$(hand::parse "$2")"
@@ -85,33 +69,5 @@ compare_high_card2() {
         echo loose
     fi
 }
-
-# hand::group() {
-#     local -A group
-#     local hand=$1
-#     local -a cards
-#     read -ra cards <<<"$hand"
-
-#     for card in "${cards[@]}"; do
-#         ((group[${card%?}] += 1))
-#     done
-# }
-
-# declare -a Cards=(2 3 4 5 6 7 8 9 10 J Q K A)
-# declare -A Ranks
-
-# for rank in "${!Cards[@]}"; do
-#     Ranks[${Cards[$rank]}]=$rank
-# done
-# unset seq
-
-# sort_hand() {
-#     local -n __sorted=$2
-#     local -a cards
-#     read -ra cards <<<"$1"
-#     mapfile -t __sorted < <(for card in "${cards[@]}"; do
-#         echo "${Ranks[${card%?}]}${card:(-1)}" # <rank> <suit>
-#     done | sort -k1,1rn)
-# }
 
 main "$@"
