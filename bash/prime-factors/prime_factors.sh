f8204adab096ee35e1a7d0a7213edcfe96f79c55
Weinan Li <liweinan0423@icloud.com>
2022-02-17 16:05:59 +0800

    --wip--

--- bash/prime-factors/prime_factors.sh
+++ bash/prime-factors/prime_factors.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 main() {
-    :
+    quotient=$1
+    ((quotient == 1)) && return
+    local -a factors
+    while ((quotient > 1)); do
+        for ((i = 2; i <= $1; i++)); do
+            if ((quotient % i == 0)); then
+                factors+=("$i")
+                ((quotient = quotient / i))
+                break
+            fi
+        done
+    done
+
+    ((${#factors[@]} == 0)) && factors+=("$1")
+
+    echo "${factors[@]}"
 }
+
 main "$@"
