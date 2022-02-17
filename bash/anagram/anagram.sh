7de9462af5c0b1ebf1d789a8fff0bcd2d62c6d1b
Weinan Li <liweinan0423@icloud.com>
2022-02-17 22:17:07 +0800

    --wip--

--- bash/anagram/anagram.sh
+++ bash/anagram/anagram.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 main() {
     local target=$1
-    local -a candidates
     read -ra candidates <<<"$2"
-    local -a anagrams
-    for word in "${candidates[@]}"; do
-        shopt -s nocasematch
-        if [[ ${#word} -eq ${#target} && $word != "$target" ]]; then
-            local buf=$word
-            for ((idx = 0; idx < ${#target}; idx++)); do
-                char=${target:$idx:1}
-                buf=${buf/$char/}
-            done
-            [[ -z $buf ]] && anagrams+=("$word")
-        fi
-    done
-
+    set -- "${candidates[@]}"
+    for word; do is_anagram && anagrams+=("$word"); done
     echo "${anagrams[@]}"
+}
+
+is_anagram() {
+    shopt -s nocasematch
+    if [[ ${#word} -eq ${#target} && $word != "$target" ]]; then
+        local buf=$word
+        for ((idx = 0; idx < ${#target}; idx++)); do
+            char=${target:$idx:1}
+            buf=${buf/$char/}
+        done
+        [[ -z $buf ]]
+    else
+        return 1
+    fi
 }
 
 main "$@"
