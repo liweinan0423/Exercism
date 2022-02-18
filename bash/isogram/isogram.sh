2d97a28479f8c023e40fea89c3dd4de1153e2395
Weinan Li <liweinan0423@icloud.com>
2022-02-18 10:45:13 +0800

    --wip--

--- bash/isogram/isogram.sh
+++ bash/isogram/isogram.sh
@@ -7,4 +7,5 @@ while IFS= read -rn1 char; do
     ((counters[$char]++))
 done < <(printf "%s" "${1//[\ -]/}")
 
-IFS=; [[ ${counters[*]} =~ ^[1]*$ ]] && echo true || echo false
+IFS=
+[[ ${counters[*]} =~ ^1*$ ]] && echo true || echo false
