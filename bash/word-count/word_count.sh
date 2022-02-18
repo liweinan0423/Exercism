1e2d77621aa4ae437805755bbe3da73744fa2bc0
Weinan Li <liweinan0423@icloud.com>
2022-02-18 17:02:51 +0800

    --wip--

--- bash/word-count/word_count.sh
+++ bash/word-count/word_count.sh
@@ -8,14 +8,21 @@ phrase=${phrase//"\n"/\ } # replace literal "\n" with space
 phrase=${phrase//[,*]/\ } # replace all delimeters with space
 
 declare -A stats
+
+
+# arithmetic expression breaks if there is single quote in it, so we have to assign the value use a variable to do the math separately
+incr() {
+    local -i count=${stats[$1]}
+    stats[$1]=$((count + 1))
+}
+
 for word in $phrase; do
     if [[ -n $word ]]; then
         word=${word#\'}            # remote leading single quote
         word=${word%\'}            # remove trailing single quote
         word=${word//[^a-z0-9\']/} # remove special characters
 
-        count=${stats[$word]}
-        stats[$word]=$((count + 1))
+        incr "$word"
     fi
 done
 
