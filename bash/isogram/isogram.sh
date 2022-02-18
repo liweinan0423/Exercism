91d7fc7168ee76a1395a455780a08c41de5a8e38
Weinan Li <liweinan0423@icloud.com>
2022-02-18 10:43:12 +0800

    --wip--

--- bash/isogram/isogram.sh
+++ bash/isogram/isogram.sh
@@ -4,7 +4,7 @@ declare -A counters
 
 declare -l char
 while IFS= read -rn1 char; do
-    [[ $char != [\ -] ]] && ((counters[$char]++))
-done < <(printf "%s" "$1")
+    ((counters[$char]++))
+done < <(printf "%s" "${1//[\ -]/}")
 
 [[ $(printf "%s" "${counters[@]}") =~ ^1*$ ]] && echo true || echo false
