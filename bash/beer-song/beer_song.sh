bdddf1cbe139cd2864fed97d7412d324dab24020
Weinan Li <liweinan0423@icloud.com>
2022-02-21 15:49:22 +0800

    --wip--

--- bash/beer-song/beer_song.sh
+++ bash/beer-song/beer_song.sh
@@ -73,32 +73,24 @@ state_machine() {
             bottles_left="1 bottle"
             take="take one down"
             then="pass it around"
-            # echo "2 bottles of beer on the wall, 2 bottles of beer."
-            # echo "Take one down and pass it around, 1 bottle of beer on the wall."
             ;;
         1)
             bottles="1 bottle"
             take="Take it down"
             then="pass it around"
             bottles_left="no more bottles"
-            # echo "1 bottle of beer on the wall, 1 bottle of beer."
-            # echo "Take it down and pass it around, no more bottles of beer on the wall."
             ;;
         0)
             bottles="no more bottles"
             take="go to the store"
             then="buy some more"
             bottles_left="99 bottles"
-            # echo "No more bottles of beer on the wall, no more bottles of beer."
-            # echo "Go to the store and buy some more, 99 bottles of beer on the wall."
             ;;
         *)
             bottles="$state bottles"
             take="take one down"
             then="pass it around"
             bottles_left="$((state - 1)) bottles"
-            # echo "$state bottles of beer on the wall, $state bottles of beer."
-            # echo "Take one down and pass it around, $((state - 1)) bottles of beer on the wall."
             ;;
         esac
 
