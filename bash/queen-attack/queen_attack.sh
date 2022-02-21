ff02b91ee180afdbe5db56cb4b95cc9afc09fbd8
Weinan Li <liweinan0423@icloud.com>
2022-02-21 16:53:28 +0800

    --wip--

--- bash/queen-attack/queen_attack.sh
+++ bash/queen-attack/queen_attack.sh
@@ -14,12 +14,15 @@ main() {
         *) usage ;;
         esac
     done
-
-    ((w[0] >= 0 && b[0] >= 0)) || die "row not positive"
-    ((w[1] >= 0 && b[1] >= 0)) || die "column not positive"
+    positive_row &&
+        ((w[1] >= 0 && b[1] >= 0)) || die "column not positive"
     ((w[0] < 8 && b[0] < 8)) || die "row not on board"
     ((w[1] < 8 && b[1] < 8)) || die "column not on board"
     ((w[0] != b[0] && w[1] && b[1])) || die "same position"
+}
+
+positive_row() {
+    ((w[0] >= 0 && b[0] >= 0)) || die "row not positive"
 }
 
 die() {
