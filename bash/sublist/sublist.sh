9075b8e44a3da9f8b17e557b1446e4928a3f6b5f
Weinan Li <liweinan0423@icloud.com>
2022-02-21 12:14:17 +0800

    --wip--

--- bash/sublist/sublist.sh
+++ bash/sublist/sublist.sh
@@ -5,28 +5,47 @@ main() {
     parse a "$1"
     parse b "$2"
 
-    compare a b
+    if is_sublist a b && is_sublist b a; then
+        echo equal
+    elif is_sublist a b && ! is_sublist b a; then
+        echo sublist
+    elif ! is_sublist a b && is_sublist b a; then
+        echo superlist
+    else
+        echo unequal
+    fi
 }
 
 parse() {
     local -n __array=$1
     local IFS
-    IFS=, read -ra __array <<<"${2//[\[\][:space:]]/}"
+    IFS=, read -ra __array <<<"${2//[\[\]]/}"
 }
 
-compare() {
+is_sublist() {
     local -n __left=$1 __right=$2
-    local left=${__left[*]} right=${__right[*]}
 
-    if [[ $left == *$right* && $right == *$left* ]]; then
-        echo equal
-    elif [[ $left == *$right* && ! $right == *$left* ]] && ((${#__left[@]} > ${#__right[@]})); then
-        echo superlist
-    elif [[ ! $left == *$right* && $right == *$left* ]] && ((${#__left[@]} < ${#__right[@]})); then
-        echo sublist
-    else
-        echo unequal
+    local sublist=true
+
+    local -i pL=0 pR=0 start=0
+    while ((pL < ${#__left[@]} && pR < ${#__right[@]})); do
+        if [[ ${__left[pL]} -eq ${__right[pR]} ]]; then
+            sublist=true
+            ((pL++))
+            ((pR++))
+        else
+            sublist=false
+            pL=0
+            ((start++))
+            pR=$start
+        fi
+    done
+
+    if ((pL != ${#__left[@]})); then
+        sublist=false
     fi
+
+    $sublist
 }
 
 main "$@"
