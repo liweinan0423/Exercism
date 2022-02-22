8315e01631c5a6eb7b2d70770e715761e87c63de
Weinan Li <liweinan0423@icloud.com>
2022-02-22 23:03:58 +0800

    --wip--

--- bash/sum-of-multiples/sum_of_multiples.sh
+++ bash/sum-of-multiples/sum_of_multiples.sh
@@ -4,12 +4,12 @@ max=$1
 shift
 sum=0
 multiples=()
-for i; do
-    ((i > 0)) || continue
-    for ((j = i; j < max; j += i)); do
-        if ! ((multiples[j])); then
-            ((sum += j))
-            multiples[$j]=1
+for base; do
+    ((base > 0)) || continue
+    for ((i = base; i < max; i += base)); do
+        if ! ((multiples[i])); then
+            ((sum += i))
+            multiples[$i]=1
         fi
     done
 done
