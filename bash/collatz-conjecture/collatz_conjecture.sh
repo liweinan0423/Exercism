e956234d09ab9d96506c9bafd85ca62837ef23d5
Weinan Li <liweinan0423@icloud.com>
2022-02-22 14:08:39 +0800

    --wip--

--- bash/collatz-conjecture/collatz_conjecture.sh
+++ bash/collatz-conjecture/collatz_conjecture.sh
@@ -9,6 +9,7 @@ declare -i number=$1 step=0
 ((number > 0)) || die "Error: Only positive numbers are allowed"
 
 until ((number == 1)); do
+    ((number > 0)) || die "Error: Number Overflow: $number, try a smaller number"
     if ((number % 2 == 0)); then
         ((number /= 2))
     else
