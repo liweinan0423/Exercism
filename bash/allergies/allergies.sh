c140ccc99745dfb08e23a80340d35a62cf4b79f0
Weinan Li <liweinan0423@icloud.com>
2022-02-22 17:00:43 +0800

    --wip--

---
+++ bash/allergies/allergies.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+readonly -A Scores=(
+    [eggs]=1
+    [peanuts]=2
+    [shellfish]=4
+    [strawberries]=8
+    [tomatoes]=16
+    [chocolate]=32
+    [pollen]=64
+    [cats]=128
+)
+
+allergic_to() {
+    ((${Scores[$2]} == $1))
+}
+
+main() {
+    if allergic_to "$1" "$3"; then
+        echo true
+    else
+        echo false
+    fi
+}
+
+main "$@"
