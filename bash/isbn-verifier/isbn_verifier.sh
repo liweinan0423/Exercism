2738cda062147ae787a144b2fa33508538f6acb0
Weinan Li <liweinan0423@icloud.com>
2022-02-24 13:41:06 +0800

    --wip--

---
+++ bash/isbn-verifier/isbn_verifier.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+digits=${1//-/}
+
+declare -i sum
+for ((i = 0; i < ${#digits}; i++)); do
+    case ${digits:i:1} in
+    [0-9])
+        ((sum += ${digits:i:1} * (10 - i)))
+        ;;
+    X)
+        ((sum += 10))
+        ;;
+    *)
+        echo false
+        exit
+        ;;
+    esac
+done
+
+((sum % 11 == 0)) && echo true || echo false
