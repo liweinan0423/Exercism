a813b32d5956d1651326365f15901a942e6338dc
Weinan Li <liweinan0423@icloud.com>
2022-02-25 14:34:15 +0800

    --wip--

--- bash/diffie-hellman/diffie_hellman.sh
+++ bash/diffie-hellman/diffie_hellman.sh
@@ -7,37 +7,24 @@ main() {
 }
 
 privateKey() {
-    local -i p=$1 r=0
-    until prime "$r" && ((r < p)); do
-        r=$((RANDOM % p + 1))
+    local -i p=$1 r
+
+    while ((r < 2)); do
+        r=$((RANDOM % p))
     done
 
-    echo $r
+    echo "$r"
 }
 
 publicKey() {
     local -i p=$1 g=$2 private=$3
-
-    echo $((g ** private % p))
+    echo $(((g ** private) % p))
 }
 
 secret() {
     local -i p=$1 public=$2 private=$3
 
     echo $((public ** private % p))
-}
-
-prime() {
-    local -i n=$1 i
-    ((n < 2)) && return 1
-    ((n == 2)) && return 0
-    for ((i = 2; i < n; i++)); do
-        if ((n % i == 0)); then
-            return 1
-        fi
-    done
-
-    return 0
 }
 
 main "$@"
