52bf773e9a7a87bc8752bfa89fffe626f49f7958
Weinan Li <liweinan0423@icloud.com>
2022-02-26 21:22:46 +0800

    --wip--

--- bash/rational-numbers/rational_numbers.sh
+++ bash/rational-numbers/rational_numbers.sh
@@ -53,8 +53,14 @@ function multiply {
 }
 
 rational_number() {
-    local -i numerator=$1 demoninator=$2
-    ((numerator == 0)) && demoninator=1
+    local -i numerator=$1 demoninator=$2 gcd
+    if ((numerator == 0)); then
+        demoninator=1
+    else
+        gcd=$(gcd $numerator $demoninator)
+        ((numerator /= gcd))
+        ((denominator /= gcd))
+    fi
     echo "$numerator/$demoninator"
 }
 
@@ -68,6 +74,32 @@ demoninator() {
     if [[ $1 =~ .*/(.*) ]]; then
         echo "${BASH_REMATCH[1]}"
     fi
+}
+
+abs() {
+    if (($1 >= 0)); then
+        echo $1
+    else
+        echo $((-$1))
+    fi
+}
+
+gcd() {
+    local -i a b min
+    a=$(abs $1)
+    b=$(abs $2)
+    ((a <= b)) && min=$a || min=$b
+    ((min != 0)) || return 1
+    local -i i
+    for ((i = min; i > 0; i--)); do
+        if ((a % i == 0 && b % i == 0)); then
+            echo $i
+            return
+        fi
+    done
+
+    echo "Why are you here?" >&2
+    return 1
 }
 
 main "$@"
