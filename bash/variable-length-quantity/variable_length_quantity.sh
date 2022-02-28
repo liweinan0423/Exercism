f44bc69ee19521cd553f4da8d07bc84cc478a2b1
Weinan Li <liweinan0423@icloud.com>
2022-02-28 13:01:38 +0800

    --wip--

--- bash/variable-length-quantity/variable_length_quantity.sh
+++ bash/variable-length-quantity/variable_length_quantity.sh
@@ -9,7 +9,23 @@ main() {
 }
 
 encode() {
-    echo $1
+    local decimal=$((16#$1))
+    local -a digits=()
+    ((decimal == 0)) && digits=(0)
+    while ((decimal > 0)); do
+        digits=("$((decimal % 128))" "${digits[@]}")
+        ((decimal /= 128))
+    done
+
+    for ((i = 0; i < ${#digits[@]}; i++)); do
+        if ((i != ${#digits[@]} - 1)); then
+            printf -v digits[i] "%02X" $((digits[i] ^ 128))
+        else
+            printf -v digits[i] "%02X" "${digits[i]}"
+        fi
+    done
+
+    echo "${digits[@]}"
 }
 
 main "$@"
