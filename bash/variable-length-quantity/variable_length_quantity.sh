ce76856d33fb517f3aaaedff2b74e8451729eb40
Weinan Li <liweinan0423@icloud.com>
2022-02-28 17:06:08 +0800

    --wip--

--- bash/variable-length-quantity/variable_length_quantity.sh
+++ bash/variable-length-quantity/variable_length_quantity.sh
@@ -27,7 +27,7 @@ encode_number() {
     local msb=0
     local -a bytes
     while true; do
-        bytes=("$(printf "%02X" $((decimal & MASK | msb)))" "${bytes[@]}")
+        bytes=("$(printf "%02X" $((decimal & MASK ^ msb)))" "${bytes[@]}")
         msb=0x80 # 10000000
         ((decimal >>= 7))
         ((decimal == 0)) && break
