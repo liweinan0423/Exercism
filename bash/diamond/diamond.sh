79cc9efba1d15270f9fb477d1f6220efe38a4ef8
Weinan Li <liweinan0423@icloud.com>
2022-03-01 15:03:38 +0800

    --wip--

---
+++ bash/diamond/diamond.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+declare -ra Alphabets=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+declare -A Indexes
+
+for index in "${!Alphabets[@]}"; do
+    Indexes[${Alphabets[$index]}]=$index
+done
+
+end=${Indexes[$1]}
+
+declare -i i=0
+declare forward=true
+
+while ((i > -1)); do
+    printf "%*s" $((end - i)) ""
+    printf "%s" "${Alphabets[$i]}"
+    if ((i == 0)); then
+        printf "%*s\n" $((end - i)) ""
+    else
+        printf "%*s" $((2 * i - 1)) ""
+        printf "%s" "${Alphabets[$i]}"
+        printf "%*s\n" $((end - i)) ""
+    fi
+    ((i == end)) && forward=false
+    $forward && ((i += 1))
+    $forward || ((i -= 1))
+done
