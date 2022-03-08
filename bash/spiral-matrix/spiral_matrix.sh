452bd9e341d281f19e53e6f0122a103bd6cb6e33
Weinan Li <liweinan0423@icloud.com>
2022-03-08 12:17:28 +0800

    --wip--

--- bash/spiral-matrix/spiral_matrix.sh
+++ bash/spiral-matrix/spiral_matrix.sh
@@ -1,24 +1,53 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+dir=east
+width=$1
+
+x=0 y=0
+
+offsetX=0 offsetY=0
+
+declare -a matrix
+for ((i = 1; i <= $1 * $1; i++)); do
+    matrix[$((y * $1 + x))]=$i
+    case $dir in
+    east)
+        ((x += 1))
+        if ((x - offsetX == width - 1)); then
+            dir=south
+            ((reset += 1))
+        fi
+        ;;
+    south)
+        ((y += 1))
+        if ((y - offsetY == width - 1)); then
+            dir=west
+            ((reset += 1))
+        fi
+        ;;
+    west)
+        ((x -= 1))
+        if ((x == offsetX)); then
+            dir=north
+            ((reset += 1))
+        fi
+        ;;
+    north)
+        ((y -= 1))
+        if ((y == offsetY + 1)); then
+            dir=east
+            ((reset += 1))
+        fi
+        ;;
+    esac
+    if ((reset == 4)); then
+        reset=0
+        ((width -= 2))
+        ((offsetX += 1))
+        ((offsetY += 1))
+    fi
+done
+
+for ((i = 0; i < ${#matrix[@]}; i += $1)); do
+    echo "${matrix[@]:i:$1}"
+done
